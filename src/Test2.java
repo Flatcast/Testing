fcde5159b8b7aa3f8b0ec742b1d671ecc8a3484d
Flatcast <1984vgl@gmail.com>
2023-06-23 14:29:24 +0300

    230623-0923

--- src/Test2.java
+++ src/Test2.java
@@ -3,15 +3,32 @@ import java.util.Scanner;
 
 public class Test2 {
     public static void main(String[] args) {
-
-
-        System.out.println();
         int rows = 7;
-        int rowBusy = 2;
-        if (rows %2==0){
-            System.out.println(((rows / 2) < rowBusy) ? "Ticket price: $10" : "Ticket price: $8");
-        }else
-        System.out.println((((rows -1)/ 2) < rowBusy) ? "Ticket price: $10" : "Ticket price: $8");
+        int seatsPerRow = 7;
+        int rowBusy = 3;
+        int seatBusy = 4;
+        int[][] busySeats = new int[rows +1][seatsPerRow +1];
+        busyPlaces(rows, seatsPerRow, rowBusy, seatBusy, busySeats);
+        rowBusy = 5;
+        seatBusy = 7;
+        busyPlaces(rows, seatsPerRow, rowBusy, seatBusy, busySeats);
+    }
 
+    public static void busyPlaces(int rows, int seatsPerRow, int rowBusy, int seatBusy,int[][] busySeats) {
+        busySeats[rowBusy][seatBusy] = 1;
+        System.out.println();
+        System.out.println("Cinema:");
+        System.out.print("  ");
+        for (int i = 1; i <= seatsPerRow; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j <= seatsPerRow; j++){
+                System.out.print(busySeats[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
